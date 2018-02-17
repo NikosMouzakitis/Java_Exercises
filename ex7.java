506087fb98858638f25e4cbf33b0251929ec7cc7
NikosMouzakitis <mzktsn@gmail.com>
2018-02-18 01:30:23 +0200

    Create ex7.java

---
+++ ex7.java
@@ -0,0 +1,11 @@
+public class ex7 {
+	public static void main(String[] args) {
+		Polygon p = new Polygon(2,4);
+		p.print_Area();	
+		
+
+		Polygon aplo = new Polygon();
+		aplo.print_Area();
+
+	}
+}
