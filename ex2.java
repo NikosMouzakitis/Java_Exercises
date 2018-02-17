aeae0648a2f57acbb5869f9df344606d8c761e0a
NikosMouzakitis <mzktsn@gmail.com>
2018-02-17 09:40:44 +0200

    Create ex2.java

---
+++ ex2.java
@@ -0,0 +1,20 @@
+// Declaring an array , giving it values and printing out.
+public class ex2{
+	public static void main(String[] str) {
+		int[] pin;	
+		int i;
+
+		pin = new int[4];
+
+		pin[0] = 2;
+		pin[1] = 3;
+		pin[2] = 4;
+		pin[3] = 5;	
+		
+		for( i = 0 ; i < 4; i++)	
+			System.out.println(pin[i]);
+
+
+	}
+
+}
